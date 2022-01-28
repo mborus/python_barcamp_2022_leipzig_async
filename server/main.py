8bd3766dfe39bdf70f4f08d098f72eb9f1da5934
Martin Borus <martin@borus.de>
2022-01-29 00:42:33 +0100

    cleanup

--- server/main.py
+++ server/main.py
@@ -11,7 +11,6 @@ from background import background_worker_thread
 LOOP_TIME_WAIT_SEC = 0.2
 LOOP_TIMEOUT = 5
 PQ_MAXSIZE = 10
-VERBOSE = False
 
 global_pq = PriorityQueue()  # do not use maxsize, it blocks!
 global_task_count = 0
@@ -31,29 +30,28 @@ async def root():
     mytask = MyTask(no=global_task_count)
 
     # check queue size
-    size_pq = len(global_pq.queue)
-    if size_pq > PQ_MAXSIZE:
+    if len(global_pq.queue) > PQ_MAXSIZE:
         raise HTTPException(status_code=429, detail="worker queue busy")
 
-    print(f"put {mytask} into queue, size {size_pq}")
+    print(f"put {mytask} into queue, size {len(global_pq.queue)}")
     global_pq.put((mytask.priority, mytask))
 
     while True:
-        if VERBOSE:
-            print(f"wait for, {mytask} status {mytask.completed}")
+
         await asyncio.sleep(LOOP_TIME_WAIT_SEC)
+
         if mytask.completed:
             break
+
         if mytask.exist_time > LOOP_TIMEOUT:
             mytask.aborted = True  # Race condition with background worker!
             raise HTTPException(status_code=408, detail="worker did not finish on time")
 
-    size_pq = sum([1 for _ in global_pq.queue])
     return {
         "count": mytask.no,
         "global_count": global_task_count,
         "priority": mytask.priority,
-        "queue_size": size_pq,
+        "queue_size": len(global_pq.queue),
         "runtime_ms": int(mytask.exist_time * 1000),
     }
 
